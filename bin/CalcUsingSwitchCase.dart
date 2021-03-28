42652b9f19422143a39f18f2b9b2c4f1b09c97ab
Amr Yousre <master_j_20@hotmail.com>
2021-03-28 14:49:19 +0200

    Calc using switch case

--- bin/CalcUsingSwitchCase.dart
+++ bin/CalcUsingSwitchCase.dart
@@ -1,16 +1,17 @@
 import 'dart:io';
 
 void main() {
-var choose;
-var number1;
-var number2;
-print('''Please Enter Your Option:
+var choose;                                               //var declaration
+var number1;                                              //var declaration
+var number2;                                              //var declaration
+//option given to the user to choose which operator he want
+print('''Please Enter Your Option:                       
 1-To Sum Two Numbers.
 2-To Subtract Two numbers.
 3-To Multiple Two Numbers.
 4-To Divide Two numbers.''');
-choose = int.parse(stdin.readLineSync());
-switch (choose) {
+choose = int.parse(stdin.readLineSync());                           // initialization for the choose value
+switch (choose) {                                                   // check the choose the user entered and check which case
   case 1 :
     print('Please Enter Your First Number:');
     number1 = int.parse(stdin.readLineSync());
@@ -39,11 +40,7 @@ switch (choose) {
     number2 = int.parse(stdin.readLineSync());
     print('$number1 / $number2 = ${(number1/number2)}');
     break;
-  default:
+  default:                                                    // check the user if he entered a wrong option give him this error message
     print('Invalid Option!!!');
-
-
-
 }
-
 }
